6ed5bc9ec165d3dc2eb735a0e3ab015a092cba50
jeoeveren <jeroenvanoeveren@hotmail.com>
2017-07-01 09:16:01 +0200

    Add dinosaur validations

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,3 +1,7 @@
 class Dinosaur < ApplicationRecord
-
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :age, length: { maximum: 5 }, allow_blank: true
+  validates :description, format: { with: /\A[a-zA-Z]+\z/,
+    message: "Required attribute and only allows letters" }, allow_blank: false
 end
